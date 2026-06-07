42fb97a2467018b748d14891c96d06b8b19db575
prathamesh gorad <prathameshgorad96@gmail.com>
2026-06-07 23:54:04 +0530

    Added Morgan and Winston logging

--- test/mvc and sesssion/app.js
+++ test/mvc and sesssion/app.js
@@ -4,11 +4,14 @@ const router = require('./route/userrouter')
 const {connectDB} = require('./db')
 const methodOverride = require('method-override');
 const session = require('express-session');
+const morgan = require('morgan');
+
 
 
 connectDB()
 const app = express()
 app.use(express.json())
+app.use(morgan('short'))
 app.use(session({
     secret:"zyxgchsbdjshv",
     resave:false,
--- test/mvc and sesssion/controller/usercontroller.js
+++ test/mvc and sesssion/controller/usercontroller.js
@@ -1,106 +1,246 @@
-const rmodel = require('../model/rmodel')
-const umodel = require('../model/umodel')
+const rmodel = require('../model/rmodel');
+const umodel = require('../model/umodel');
 const bcryptjs = require('bcryptjs');
+const logger = require('../logger');
 
-const addform=(req,resp)=>{
-    resp.render("add")
-}
+// Add Room Form
+const addform = (req, res) => {
+    logger.info("Add room page opened");
+    res.render("add");
+};
 
-const showRoom = async(req,resp)=>{
-    if(!req.session.name){
-         return resp.redirect("/login");
+// Show Rooms
+const showRoom = async (req, res) => {
+
+    if (!req.session.name) {
+        logger.warn("Unauthorized access to showRoom");
+        return res.redirect("/login");
     }
+
     try {
-        const data = await rmodel.find()
-        resp.render("show",{data,username:req.session.name})
+
+        const data = await rmodel.find();
+
+        logger.info(
+            `${req.session.name} viewed rooms`
+        );
+
+        res.render("show", {
+            data,
+            username: req.session.name
+        });
+
     } catch (error) {
-        console.log(error);
-        
-        
-    }
-}
 
-const addRoom = async(req,resp)=>{
+        logger.error(
+            `Show Room Error: ${error.message}`
+        );
+
+        res.send("Error loading rooms");
+    }
+};
+
+// Add Room
+const addRoom = async (req, res) => {
+
     try {
+
         const data = new rmodel({
-            roomnumber:req.body.roomnumber,
-            roomtype:req.body.roomtype,
-            pricepernight:req.body.pricepernight,
-            capacity:req.body.capacity
-        })
-        await data.save()
-        resp.redirect("/showRoom")
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
-const deleteRoom =async(req,resp)=>{
-    try {
-        await rmodel.findByIdAndDelete(req.params.id)
-        resp.redirect("/showRoom")
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
+            roomnumber: req.body.roomnumber,
+            roomtype: req.body.roomtype,
+            pricepernight: req.body.pricepernight,
+            capacity: req.body.capacity
+        });
 
-const updateRoom = async(req,resp)=>{
-    try {
-        await rmodel.findByIdAndUpdate(req.params.id,req.body)
-        resp.redirect("/showRoom")
+        await data.save();
+
+        logger.info(
+            `Room ${req.body.roomnumber} created`
+        );
+
+        res.redirect("/showRoom");
+
     } catch (error) {
-        console.log(error);
-        
-    }
-}
 
-const editform = async(req,resp)=>{
-    try{
-        const data = await rmodel.findById(req.params.id)
-        resp.render("edit",{data})
-    }
-    catch(error){
-        console.log(error);
-        
-    }
-}
+        logger.error(
+            `Add Room Error: ${error.message}`
+        );
 
-const register = async(req,resp)=>{
-    try {
-        const {name,email,password} = req.body
-        const hashpassword = await bcryptjs.hash(password,10)
-        await umodel.create({name,email,password:hashpassword})
-        resp.redirect("/login")
-    } catch (error) {
-        console.log(error);
-        
-    }
-}
-
-const login = async(req,resp)=>{
-    try {
-        const {name,password} = req.body
-        const user = await umodel.findOne({name})
-        if(user && await bcryptjs.compare(password,user.password)){
-            req.session.name = name
-            resp.redirect("/showRoom")
+        if (error.code === 11000) {
+            return res.send("Room number already exists");
         }
-        else{
-            resp.redirect("/login")
-        }
-        
-    } catch (error) {
-        console.log(error);
-        
+
+        res.send("Failed to add room");
     }
-}
+};
 
-const logout = async(req,resp)=>{
-    req.session.destroy(()=>{
-        resp.redirect("/login")
-    })
-    
-}
+// Delete Room
+const deleteRoom = async (req, res) => {
 
-module.exports = {addform,showRoom,addRoom,deleteRoom,updateRoom,editform,register,login,logout}
+    try {
+
+        const room = await rmodel.findById(req.params.id);
+
+        await rmodel.findByIdAndDelete(req.params.id);
+
+        logger.info(
+            `Room ${room.roomnumber} deleted`
+        );
+
+        res.redirect("/showRoom");
+
+    } catch (error) {
+
+        logger.error(
+            `Delete Room Error: ${error.message}`
+        );
+
+        res.send("Failed to delete room");
+    }
+};
+
+// Update Room
+const updateRoom = async (req, res) => {
+
+    try {
+
+        await rmodel.findByIdAndUpdate(
+            req.params.id,
+            req.body
+        );
+
+        logger.info(
+            `Room updated: ${req.params.id}`
+        );
+
+        res.redirect("/showRoom");
+
+    } catch (error) {
+
+        logger.error(
+            `Update Room Error: ${error.message}`
+        );
+
+        res.send("Failed to update room");
+    }
+};
+
+// Edit Form
+const editform = async (req, res) => {
+
+    try {
+
+        const data = await rmodel.findById(req.params.id);
+
+        logger.info(
+            `Edit room page opened: ${req.params.id}`
+        );
+
+        res.render("edit", { data });
+
+    } catch (error) {
+
+        logger.error(
+            `Edit Form Error: ${error.message}`
+        );
+
+        res.send("Failed to open edit page");
+    }
+};
+
+// Register
+const register = async (req, res) => {
+
+    try {
+
+        const { name, email, password } = req.body;
+
+        const hashpassword = await bcryptjs.hash(password, 10);
+
+        await umodel.create({
+            name,
+            email,
+            password: hashpassword
+        });
+
+        logger.info(
+            `New user registered: ${name}`
+        );
+
+        res.redirect("/login");
+
+    } catch (error) {
+
+        logger.error(
+            `Register Error: ${error.message}`
+        );
+
+        res.send("Registration failed");
+    }
+};
+
+// Login
+const login = async (req, res) => {
+
+    try {
+
+        const { name, password } = req.body;
+
+        const user = await umodel.findOne({ name });
+
+        if (
+            user &&
+            await bcryptjs.compare(
+                password,
+                user.password
+            )
+        ) {
+
+            req.session.name = name;
+
+            logger.info(
+                `${user.name} logged in successfully`
+            );
+
+            return res.redirect("/showRoom");
+        }
+
+        logger.warn(
+            `Failed login attempt for ${name}`
+        );
+
+        res.redirect("/login");
+
+    } catch (error) {
+
+        logger.error(
+            `Login Error: ${error.message}`
+        );
+
+        res.send("Login failed");
+    }
+};
+
+// Logout
+const logout = (req, res) => {
+
+    logger.info(
+        `${req.session.name} logged out`
+    );
+
+    req.session.destroy(() => {
+        res.redirect("/login");
+    });
+};
+
+module.exports = {
+    addform,
+    showRoom,
+    addRoom,
+    deleteRoom,
+    updateRoom,
+    editform,
+    register,
+    login,
+    logout
+};
